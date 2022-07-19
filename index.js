f89d18e49a30214470e53e256fb568f7242970a3
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-07-19 02:12:14 -0500

    Oak added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World with Oak!";
 });
+
+await app.listen({ port: 8000 });
