ae247a29386d484df0af58971b1a70794fab4c0a
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-07-19 02:18:46 -0500

    oak rest api

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World with Oak!";
-});
+const tasks = [];
 
+const tasksRoutes = new Router()
+  .get("/", (ctx) => {
+    ctx.response.body = "Welcome to API";
+  })
+  .get("/tasks", (ctx) => {
+    ctx.response.body = tasks;
+  })
+  .post("/tasks", (ctx) => {
+    ctx.response.body = "creating task";
+  });
+
+app.use(tasksRoutes.routes());
 await app.listen({ port: 8000 });
